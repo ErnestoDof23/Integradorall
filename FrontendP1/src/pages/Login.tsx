84942078bc7ae6a8df250e7ae9e74ba35b837ce4
pilgram2393 <20243ds007@utez.edu.mx>
2026-06-25 11:48:21 -0600

    fix: button text back to Iniciar Sesión

--- FrontendP1/src/pages/Login.tsx
+++ FrontendP1/src/pages/Login.tsx
@@ -79,7 +79,7 @@ export default function Login() {
                   Ingresando...
                 </>
               ) : (
-                'Todo Accesible'
+                'Iniciar Sesión'
               )}
             </Button>
           </form>
